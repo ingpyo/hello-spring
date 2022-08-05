542986f160af7fdf8787097bd336b058bdea1e29
jeoninpyo <juninpyo726@naver.com>
2022-08-05 20:02:28 +0900

    Initial commit

---
+++ src/main/java/hello/hellospring/domain/Member.java
@@ -0,0 +1,24 @@
+package hello.hellospring.domain;
+
+import javax.persistence.*;
+
+@Entity
+public class Member {
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    //@Column(name = "username") db에 컬럼 이름이 username이면 @Column
+    private String name;
+    public Long getId() {
+        return id;
+    }
+    public void setId(Long id) {
+        this.id = id;
+    }
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+}
